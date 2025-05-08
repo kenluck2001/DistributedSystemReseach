7a17a76b883433eb19cdd9836722dc2dd53f97b3
Kenneth Odoh <kenluck2001@yahoo.com>
2025-05-07 20:06:54 -0700

    fix majority voting using lamport clock

--- textbook/lamport1-majority-voting8.c
+++ textbook/lamport1-majority-voting8.c
@@ -25,6 +25,7 @@ mpicc lamport1-majority-voting8.c && mpiexec -n 12 ./a.out
 #define TABLESIZE 150
 #define EMPTY -999
 #define KEYLENGTH 20
+#define MAXNUMOFTHREADS 25
 
 /**
  * Routine for map data structure
@@ -53,6 +54,168 @@ int getKeyValueToMap(int **map_array, const char *key) {
 }
 //////////////////////////////////////////////////////////
 
+typedef struct data_stamp {
+    char key[KEYLENGTH];
+    int value;
+    int lamport_vec[MAXNUMOFTHREADS];
+} data;
+
+
+
+MPI_Datatype serialize_data_stamp(int num_procs) {
+    /* create a type for struct data */
+    const int nitems = 3;
+    // int          blocklengths[nitems] = {1,num_procs};
+    int blocklengths[nitems];
+
+    blocklengths[0] = KEYLENGTH;
+    blocklengths[1] = 1;
+    blocklengths[2] = num_procs;
+
+    // MPI_Datatype types[nitems] = {MPI_INT, MPI_INT};
+    MPI_Datatype types[nitems];
+    types[0] = MPI_CHAR;
+    types[1] = MPI_INT;
+    types[2] = MPI_INT;
+
+    MPI_Datatype mpi_data_type;
+    MPI_Aint offsets[nitems];
+
+    offsets[0] = offsetof(data, key);
+    offsets[1] = offsetof(data, value);
+    offsets[2] = offsetof(data, lamport_vec);
+
+    MPI_Type_create_struct(nitems, blocklengths, offsets, types,
+                               &mpi_data_type);
+    MPI_Type_commit(&mpi_data_type);
+    return mpi_data_type;
+}
+
+void initialize_process_data(int my_rank, int num_procs,
+                                 data *package) {
+    float probability = (my_rank == 0) ? 0.5 : 0.1;
+    strncpy(package->key, "KEN", KEYLENGTH);
+    package->value = (rand() > probability) ? 40 : 1;
+    for (int i = 0; i < num_procs; i++) {
+        package->lamport_vec[i] = 0;
+    }
+    package->lamport_vec[my_rank] = 1;
+}
+
+/**
+ * @brief Sends the initial data package to all other processes.
+ * @param package The data package to send.
+ * @param num_procs The total number of processes.
+ * @param mpi_data_type The MPI datatype for the data struct.
+ * @param requests Array to store MPI requests.
+ */
+void send_initial_data(data *package, int num_procs,
+                       MPI_Datatype mpi_data_type, MPI_Request *requests) {
+    for (int other_rank = 0; other_rank < num_procs; other_rank++) {
+        MPI_Isend(package, 1, mpi_data_type, other_rank, 0, MPI_COMM_WORLD,
+                  &requests[other_rank]);
+    }
+    MPI_Waitall(num_procs, requests, MPI_STATUSES_IGNORE); // Ensure all sends complete
+}
+
+/**
+ * @brief Receives and processes data from other processes to reach agreement.
+ * @param my_rank The rank of the current process.
+ * @param num_procs The total number of processes.
+ * @param mpi_data_type The MPI datatype for the data struct.
+ * @param map_array The local key-value map.
+ * @param agreed_value Pointer to the struct to store the agreed value.
+ * @param requests Array of MPI requests.
+ * @param status Array of MPI status.
+ */
+void receive_and_process_data(int my_rank, int num_procs,
+                             MPI_Datatype mpi_data_type, int *map_array,
+                             data *agreed_value,
+                             MPI_Request *requests, MPI_Status *status) {
+    int largest_timestamp = -99999;
+    int threshold = num_procs;
+    int received_count = 0;
+    int flag = 0;
+    MPI_Request recv_request;
+    data recv_data;
+    memset(&recv_data, 0, sizeof(recv_data));
+
+    MPI_Irecv(&recv_data, 1, mpi_data_type, MPI_ANY_SOURCE, MPI_ANY_TAG,
+              MPI_COMM_WORLD, &recv_request);
+
+    while (received_count < threshold) {
+        MPI_Test(&recv_request, &flag, &status[my_rank]);
+        if (flag) {
+            int source = status[my_rank].MPI_SOURCE;
+            for (int i = 0; i < num_procs; i++) {
+                recv_data.lamport_vec[my_rank] =
+                    MAX(recv_data.lamport_vec[my_rank],
+                        recv_data.lamport_vec[source]) +
+                    1;
+            }
+
+            if (largest_timestamp < recv_data.lamport_vec[source]) {
+                largest_timestamp = recv_data.lamport_vec[source];
+                *agreed_value = recv_data;
+                setKeyValueToMap(&map_array, recv_data.key, recv_data.value);
+            }
+
+            if (source != my_rank) {
+                MPI_Isend(&recv_data, 1, mpi_data_type, source, 0,
+                          MPI_COMM_WORLD, &requests[source]);
+            }
+            received_count++;
+            flag = 0; // Reset flag for the next receive
+            MPI_Irecv(&recv_data, 1, mpi_data_type, MPI_ANY_SOURCE, MPI_ANY_TAG,
+                      MPI_COMM_WORLD, &recv_request);
+        }
+        //usleep(10); // Add a small delay to avoid excessive CPU usage
+    }
+    MPI_Cancel(&recv_request);
+    MPI_Wait(&recv_request, MPI_STATUS_IGNORE);
+    MPI_Waitall(num_procs, requests, MPI_STATUSES_IGNORE); // Ensure all sends complete
+}
+
+
+
+/**
+ * @brief Prints the agreed key-value pair and retrieves and prints values from the
+ * map.
+ * @param my_rank The rank of the current process.
+ * @param agreed_value The agreed data value.
+ * @param map_array The map array.
+ */
+void print_and_retrieve_map_data(int my_rank, data *agreed_value,
+                                 int *map_array) {
+    printf("latest Agreed key: %s, value: %d at rank: %d\n",
+           agreed_value->key, agreed_value->value, my_rank);
+    char *key = agreed_value->key;
+    int store_Value = getKeyValueToMap(&map_array, key);
+
+    if (store_Value != EMPTY) {
+        printf(
+            "For key: %s, retrieved value: %i, available in map at rank: %d\n",
+            key, store_Value, my_rank);
+    } else {
+        printf("For key: %s, has no value available in map at rank: %d\n", key,
+               my_rank);
+    }
+
+    key = "dist";
+    store_Value = getKeyValueToMap(&map_array, key);
+
+    if (store_Value != EMPTY) {
+        printf(
+            "For key: %s, retrieved value: %i, available in map at rank: %d\n",
+            key, store_Value, my_rank);
+    } else {
+        printf("For key: %s, has no value available in map at rank: %d\n", key,
+               my_rank);
+    }
+}
+
+
+
 int main(int argc, char **argv) {
     int my_rank, num_procs;
     /* Initialize the infrastructure necessary for communication */
@@ -64,252 +227,37 @@ int main(int argc, char **argv) {
     /* Find out how many total processes are active */
     MPI_Comm_size(MPI_COMM_WORLD, &num_procs);
 
-    typedef struct data_stamp {
-        char key[KEYLENGTH];
-        int value;
-        int lamport_vec[num_procs];
-    } data;
-
-    MPI_Datatype serialize_data_stamp() {
-        /* create a type for struct data */
-        const int nitems = 3;
-        // int          blocklengths[nitems] = {1,num_procs};
-        int blocklengths[nitems];
-
-        blocklengths[0] = KEYLENGTH;
-        blocklengths[1] = 1;
-        blocklengths[2] = num_procs;
-
-        // MPI_Datatype types[nitems] = {MPI_INT, MPI_INT};
-        MPI_Datatype types[nitems];
-        types[0] = MPI_CHAR;
-        types[1] = MPI_INT;
-        types[2] = MPI_INT;
-
-        MPI_Datatype mpi_data_type;
-        MPI_Aint offsets[nitems];
-
-        offsets[0] = offsetof(data, key);
-        offsets[1] = offsetof(data, value);
-        offsets[2] = offsetof(data, lamport_vec);
-
-        MPI_Type_create_struct(nitems, blocklengths, offsets, types,
-                               &mpi_data_type);
-        MPI_Type_commit(&mpi_data_type);
-        return mpi_data_type;
+    if (num_procs > MAXNUMOFTHREADS) {
+        fprintf(stderr, "Must more than %d process for this example\n", num_procs);
+        MPI_Abort(MPI_COMM_WORLD, 1);
     }
 
     /* Until this point, all programs have been doing exactly the same.
        Here, we check the rank to distinguish the roles of the programs */
 
-    MPI_Datatype mpi_data_type = serialize_data_stamp();
+    MPI_Datatype mpi_data_type = serialize_data_stamp(num_procs);
+    printf("You are in rank: %i processes.\n", my_rank);
+
+    int *map_array;
+    createMapArray(&map_array, TABLESIZE, EMPTY);
+
+    data package;
+    memset(&package, 0, sizeof(package));
+
+    data agreed_value;
+    memset(&agreed_value, 0, sizeof(agreed_value));
 
     MPI_Request requests[num_procs];
     MPI_Status status[num_procs];
 
-    printf("You are in rank: %i processes.\n", my_rank);
+    initialize_process_data(my_rank, num_procs, &package);
+    send_initial_data(&package, num_procs, mpi_data_type, requests);
+    receive_and_process_data(my_rank, num_procs, mpi_data_type, map_array,
+                             &agreed_value, requests, status);
+    print_and_retrieve_map_data(my_rank, &agreed_value, map_array);
 
-    if (my_rank == 0) {
-        printf("We have %i processes.\n", num_procs);
-        srand((my_rank + 1) * time(0));
-        int largest_timestamp = -99999;
-        float probability = 0.5;
-        int threshold = num_procs;  // set the level of resilience
-        int cnt = 0;                // number of messages
-        int flag = -1;
-        int ret;
-        char *key;
-        int *map_array;  // store map here
+    free(map_array);
 
-        createMapArray(&map_array, TABLESIZE,
-                       EMPTY);  // initialize the map here
-
-        data package;
-        memset(&package, 0, sizeof(package));
-        // create your data here
-        strncpy(package.key, "KEN", KEYLENGTH);
-        package.value = (rand() > probability) ? 40 : 1;
-        package.lamport_vec[my_rank] = package.lamport_vec[my_rank] + 1;
-
-        setKeyValueToMap(&map_array, package.key,
-                         package.value);  // add key, value pair to the map
-
-        for (int other_rank = 0; other_rank < num_procs; other_rank++) {
-            MPI_Isend(&package, 1, mpi_data_type, other_rank, my_rank,
-                      MPI_COMM_WORLD, &requests[other_rank]);
-        }
-
-        data agreed_value;  // get result after consensus
-        memset(&agreed_value, 0, sizeof(agreed_value));
-
-        data recv;
-        memset(&recv, 0, sizeof(recv));
-        while (1) {
-            /* Receive message from any process */
-            if (flag != 0) {
-                ret =
-                    MPI_Irecv(&recv, 1, mpi_data_type, MPI_ANY_SOURCE,
-                              MPI_ANY_TAG, MPI_COMM_WORLD, &requests[my_rank]);
-
-                flag = 0;
-            }
-            MPI_Test(&requests[my_rank], &flag, &status[my_rank]);
-
-            if (flag != 0) {
-                if (ret == MPI_SUCCESS) {
-                    int source = status[my_rank].MPI_SOURCE;
-                    recv.lamport_vec[my_rank] = MAX(recv.lamport_vec[my_rank],
-                                                    recv.lamport_vec[source]) +
-                                                1;
-
-                    if (largest_timestamp < recv.lamport_vec[source]) {
-                        largest_timestamp = recv.lamport_vec[source];
-                        agreed_value = recv;  // copy struct for consensus value
-
-                        setKeyValueToMap(
-                            &map_array, recv.key,
-                            recv.value);  // add key, value pair to the map
-                    }
-                    if (source != my_rank) {
-                        MPI_Isend(&recv, 1, mpi_data_type, source, 0,
-                                  MPI_COMM_WORLD, &requests[source]);
-                    }
-                }
-                cnt += 1;
-                flag = -1;
-            }
-
-            if (cnt == threshold) break;
-        }
-        printf("latest Agreed key: %s, value: %d at rank: %d\n",
-               agreed_value.key, agreed_value.value, my_rank);
-        key = agreed_value.key;
-        int store_Value = getKeyValueToMap(&map_array, key);
-
-        if (store_Value != EMPTY) {
-            printf(
-                "For key: %s, retrieved value: %i, available in map at rank: "
-                "%d\n",
-                key, store_Value, my_rank);
-        } else {
-            printf("For key: %s, has no value available in map at rank: %d\n",
-                   key, my_rank);
-        }
-
-        key = "dist";
-        store_Value = getKeyValueToMap(&map_array, key);
-
-        if (store_Value != EMPTY) {
-            printf(
-                "For key: %s, retrieved value: %i, available in map at rank: "
-                "%d\n",
-                key, store_Value, my_rank);
-        } else {
-            printf("For key: %s, has no value available in map at rank: %d\n",
-                   key, my_rank);
-        }
-
-        free(map_array);  // free memory
-    } else {
-        srand((my_rank + 1) * time(0));
-        int largest_timestamp = -99999;
-        float probability = 0.1;
-        int threshold = num_procs;  // set the level of resilience
-        int cnt = 0;
-        int flag = -1;
-        int ret;
-        char *key;
-        int *map_array;  // store map here
-
-        createMapArray(&map_array, TABLESIZE,
-                       EMPTY);  // initialize the map here
-
-        data package;
-        memset(&package, 0, sizeof(package));
-        // create your data here
-        strncpy(package.key, "KEN", KEYLENGTH);
-
-        package.value = (rand() > probability) ? 4 : 1;
-        package.lamport_vec[my_rank] = package.lamport_vec[my_rank] + 1;
-
-        for (int other_rank = 0; other_rank < num_procs; other_rank++) {
-            MPI_Isend(&package, 1, mpi_data_type, other_rank, my_rank,
-                      MPI_COMM_WORLD, &requests[other_rank]);
-        }
-
-        data agreed_value;  // get result after consensus
-        memset(&agreed_value, 0, sizeof(agreed_value));
-
-        data recv;
-        memset(&recv, 0, sizeof(recv));
-
-        while (1) {
-            /* Receive message from any process */
-            if (flag != 0) {
-                ret =
-                    MPI_Irecv(&recv, 1, mpi_data_type, MPI_ANY_SOURCE,
-                              MPI_ANY_TAG, MPI_COMM_WORLD, &requests[my_rank]);
-
-                flag = 0;
-            }
-            MPI_Test(&requests[my_rank], &flag, &status[my_rank]);
-
-            if (flag != 0) {
-                if (ret == MPI_SUCCESS) {
-                    int source = status[my_rank].MPI_SOURCE;
-                    recv.lamport_vec[my_rank] = MAX(recv.lamport_vec[my_rank],
-                                                    recv.lamport_vec[source]) +
-                                                1;
-
-                    if (largest_timestamp < recv.lamport_vec[source]) {
-                        largest_timestamp = recv.lamport_vec[source];
-                        agreed_value = recv;  // copy struct for consensus value
-                        setKeyValueToMap(
-                            &map_array, recv.key,
-                            recv.value);  // add key, value pair to the map
-                    }
-
-                    if (source != my_rank) {
-                        MPI_Isend(&recv, 1, mpi_data_type, source, 0,
-                                  MPI_COMM_WORLD, &requests[source]);
-                    }
-                }
-                cnt += 1;
-                flag = -1;
-            }
-            if (cnt == threshold) break;
-        }
-
-        printf("latest Agreed key: %s, value: %d at rank: %d\n",
-               agreed_value.key, agreed_value.value, my_rank);
-        key = agreed_value.key;
-        int store_Value = getKeyValueToMap(&map_array, key);
-
-        if (store_Value != EMPTY) {
-            printf(
-                "For key: %s, retrieved value: %i, available in map at rank: "
-                "%d\n",
-                key, store_Value, my_rank);
-        } else {
-            printf("For key: %s, has no value available in map at rank: %d\n",
-                   key, my_rank);
-        }
-
-        key = "dist";
-        store_Value = getKeyValueToMap(&map_array, key);
-
-        if (store_Value != EMPTY) {
-            printf(
-                "For key: %s, retrieved value: %i, available in map at rank: "
-                "%d\n",
-                key, store_Value, my_rank);
-        } else {
-            printf("For key: %s, has no value available in map at rank: %d\n",
-                   key, my_rank);
-        }
-
-        free(map_array);  // free memory
-    }
 
     MPI_Type_free(&mpi_data_type);
     /* Tear down the communication infrastructure */
